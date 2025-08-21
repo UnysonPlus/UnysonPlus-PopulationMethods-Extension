0fa290738d8f19bb7164358738ce7161fc9fa394
jonmlas <jonmlas@gmail.com>
2025-08-21 18:59:23 +0800

    Manifest updated

--- extensions/population-method-posts/manifest.php
+++ extensions/population-method-posts/manifest.php
@@ -1,8 +1,21 @@
 <?php if (!defined('FW')) die('Forbidden');
 
+$manifest = array();
+
 $manifest['name']        = __( 'Population Method - Posts', 'fw' );
+$manifest['slug']        = 'unysonplus-population-method-posts';
 $manifest['description'] = __( 'Population Method - Posts', 'fw' );
 
-$manifest['version'] = '1.0.0';
+$manifest['version']     = '1.0.0';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
 
-$manifest['display'] = 'slider';
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
--- manifest.php
+++ manifest.php
@@ -1,7 +1,27 @@
 <?php if (!defined('FW')) die('Forbidden');
 
-$manifest['name']        = __( 'Population Methods', 'fw' );
-$manifest['description'] = '';
-$manifest['version'] = '1.0.19';
+$manifest = array();
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-PopulationMethods-Extension';
+// Basic Info
+$manifest['name']        = __( 'Population Methods', 'fw' );
+$manifest['slug']        = 'unysonplus-population-methods';
+$manifest['description'] = __( 'Provides multiple population methods (Posts, Tags, Custom, Categories, etc.) for sliders and other extensions.', 'fw' );
+
+$manifest['version']     = '1.0.19';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
+
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-PopulationMethods-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-PopulationMethods-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
